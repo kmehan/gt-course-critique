dc0425f8688007dd1e42fa717e59e3371cf3c0e1
cobookman <cobookman@gmail.com>
2013-05-15 22:40:48 -0400

    MYSQL_DATABASE_NAME is not symantically correct - Substituded MYSQL_TABLE_NAME. In regards to the two bash scripts, profSchema.sh and courseSchema.sh

--- courseSchema.sh
+++ courseSchema.sh
@@ -4,7 +4,8 @@ ELASTICSEARCH_URL = "localhost:9200"
 MYSQL_DATABASE = "192.168.1.1:3306/courseCritique"
 MYSQL_USERNAME = "*****"
 MYSQL_PASSWORD = "*****"
-MYSQL_DATABASE_NAME = "Data"
+MYSQL_TABLE_NAME = "Data"
+
 curl -XPUT "$ELASTICSEARCH_URL/_river/class/_meta" -d '{
     "type" : "jdbc",
     "jdbc" : {
@@ -12,6 +13,6 @@ curl -XPUT "$ELASTICSEARCH_URL/_river/class/_meta" -d '{
         "url" : "jdbc:mysql://$MYSQL_DATABASE",
         "user" : "$MYSQL_USERNAME",
         "password" : "$MYSQL_PASSWORD",
-        "sql" : "SELECT DISTINCT \"class\" as \"_index\", REPLACE(Course,\" \",\"\") as \"_id\", Course as \"course\", ROUND(AVG(GPA),2) as \"grades.gpa\", ROUND(AVG(A)) as \"grades.a\", ROUND(AVG(B)) as \"grades.b\", ROUND(AVG(C)) as \"grades.c\", ROUND(AVG(D)) as \"grades.d\", ROUND(AVG(F)) as \"grades.f\", ROUND(AVG(W)) as \"grades.w\"  FROM $MYSQL_DATABASE_NAME GROUP BY Course"
+        "sql" : "SELECT DISTINCT \"class\" as \"_index\", REPLACE(Course,\" \",\"\") as \"_id\", Course as \"course\", ROUND(AVG(GPA),2) as \"grades.gpa\", ROUND(AVG(A)) as \"grades.a\", ROUND(AVG(B)) as \"grades.b\", ROUND(AVG(C)) as \"grades.c\", ROUND(AVG(D)) as \"grades.d\", ROUND(AVG(F)) as \"grades.f\", ROUND(AVG(W)) as \"grades.w\"  FROM $MYSQL_TABLE_NAME GROUP BY Course"
    }
 }'
